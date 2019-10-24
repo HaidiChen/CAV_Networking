c75b8e8b2d981eb14bb755fab4ab32124e57f488
haidichen <chenhaidi43@163.com>
2019-10-23 20:01:59 -0500

    restructured class

--- extract_data.py
+++ extract_data.py
@@ -9,92 +9,96 @@ class Extractor(object):
         self._mse = defaultdict(list)
         self._ssim = defaultdict(list)
 
-    def __extract(self, path):
-        if os.path.isfile(path):
-            with open(os.path.abspath(path), 'r') as f:
+    def _extract_helper(self, path):
+        key = path.split('/')[-1]
+        with open(os.path.abspath(path), 'r') as f:
+            line = f.readline()
+            total_time, time_nums = 0, 0
+            received_files, lost_files = 0, 0
+            t_mse, t_ssim = 0, 0
+
+            while line:
+                temp_line = line.strip()
+                # extract broadcast time
+                if temp_line.startswith('Broadcasting Time'):
+                    time_nums += 1
+                    time = temp_line.split(':')[1]
+                    total_time += float(time)
+                # extract received files number
+                elif temp_line.endswith('files'):
+                    temp_string = temp_line.split(',')[1]
+                    received = temp_string.split()[0]
+                    received_files = int(received)
+                # extract lost files number
+                elif temp_line.startswith('File Loss'):
+                    lost = temp_line.split('=')[1]
+                    lost_files = int(lost)
+                # extract MSE and SSIM
+                elif temp_line.startswith('MSE'):
+                    temp_string = temp_line.split(',')
+                    mse = float(temp_string[0].split(':')[1])
+                    ssim = float(temp_string[1].split(':')[1])
+                    t_mse += mse
+                    t_ssim += ssim
                 line = f.readline()
-                total_time = 0
-                time_nums = 0
-                received_files, lost_files = 0, 0
-                t_mse, t_ssim = 0, 0
-                while line:
-                    temp_line = line.strip()
 
-                    # extract broadcast time
-                    if temp_line.startswith('Broadcasting Time'):
-                        time_nums += 1
-                        time = temp_line.split(':')[1]
-                        total_time += float(time)
+            self._set_broadcast(key, total_time, time_nums)
+            self._set_loss_rate(key, received_files, lost_files)
+            self._set_mse_ssim(key, received_files, t_mse, t_ssim)
 
-                    # extract received files number
-                    elif temp_line.endswith('files'):
-                        temp_string = temp_line.split(',')[1]
-                        received = temp_string.split()[0]
-                        received_files = int(received)
+    def _set_broadcast(self, key, total_time, time_nums):
+        if time_nums:
+            average_time = total_time / time_nums
+            self._broadcast_time[key].append(round(average_time, 5))
+    
+    def _set_mse_ssim(self, key, received, tmse, tssim):
+        if received:
+            self._mse[key].append(tmse / received)
+            self._ssim[key].append(tssim / received)
 
-                    # extract lost files number
-                    elif temp_line.startswith('File Loss'):
-                        lost = temp_line.split('=')[1]
-                        lost_files = int(lost)
+    def _set_loss_rate(self, key, received, lost):
+        desired = lost + received
+        if desired:
+            loss_rate = lost / desired
+            self._file_loss_rate[key].append(round(loss_rate, 3))
 
-                    # extract MSE and SSIM
-                    elif temp_line.startswith('MSE'):
-                        temp_string = temp_line.split(',')
-                        mse = float(temp_string[0].split(':')[1])
-                        ssim = float(temp_string[1].split(':')[1])
-                        t_mse += mse
-                        t_ssim += ssim
-
-                    line = f.readline()
-
-                key = path.split('/')[-1]
-
-                if time_nums:
-                    average_time = total_time / time_nums
-                    self._broadcast_time[key].append(round(average_time, 5))
-
-                if received_files:
-                    self._mse[key].append(t_mse / received_files)
-                    self._ssim[key].append(t_ssim / received_files)
-
-                desired_files_number = lost_files + received_files
-                if desired_files_number:
-                    loss_rate = lost_files / desired_files_number
-                    self._file_loss_rate[key].append(round(loss_rate, 3))
-
+    def _extract(self, path):
+        if os.path.isfile(path):
+            self._extract_helper(path)
+            
         elif os.path.isdir(path):
             for f in os.listdir(path):
-                self.__extract(os.path.join(path, f))
+                self._extract(os.path.join(path, f))
 
 
-    def extract_to_csv(self, path):
-        self.__extract(path)
+    def extract_to_csv_from(self, path):
+        self._extract(path)
 
         if self._broadcast_time:
             columns = list(self._broadcast_time.keys())
             columns = ','.join(columns)
             filename = 'broadcast.csv'
-            self.__write_helper(filename, columns, 0)
+            self._write_helper(filename, columns, 0)
 
         if self._file_loss_rate:
             columns = list(self._file_loss_rate.keys())
             columns = ','.join(columns)
             filename = 'file_loss_rate.csv'
-            self.__write_helper(filename, columns, 1)
+            self._write_helper(filename, columns, 1)
 
         if self._mse:
             columns = list(self._mse.keys())
             columns = ','.join(columns)
             filename = 'mse.csv'
-            self.__write_helper(filename, columns, 2)
+            self._write_helper(filename, columns, 2)
 
         if self._ssim:
             columns = list(self._ssim.keys())
             columns = ','.join(columns)
             filename = 'ssim.csv'
-            self.__write_helper(filename, columns, 3)
+            self._write_helper(filename, columns, 3)
 
-    def __write_helper(self, filename, columns, flag):
+    def _write_helper(self, filename, columns, flag):
         with open(filename, 'w') as f:
             f.write("%s\n"%(columns))
             values = []
@@ -127,7 +131,7 @@ class Extractor(object):
 def main():
     extractor = Extractor()
     print('[INFO] start extracting...')
-    extractor.extract_to_csv('log')
+    extractor.extract_to_csv_from('log')
     print('[INFO] done')
     
 
