b233e056f180e2fd83debbb2f4455d7d04de17b8
haidichen <chenhaidi43@163.com>
2019-10-24 12:55:10 -0500

    deleted dead code

--- n2otest.sh
+++ n2otest.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-#while [ 1 ]
-#do
-#  	read -p "enter to continue..." 
+tree output/ | tail -1
 
-	tree output/ | tail -1
+python3 compare.py -i desired_images/ -ii output/
 
-  	python3 compare.py -i desired_images/ -ii output/
-
-	rm output/*
-#done
+rm output/*
