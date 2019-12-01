0bd2dbbb509885e6703a466a5a6d96df202c7c8f
haidichen <chenhaidi43@163.com>
2019-12-01 13:56:44 -0600

    refactored code

--- N2NTest/extract_data.py
+++ N2NTest/extract_data.py
@@ -1,5 +1,6 @@
 import os
 from collections import defaultdict
+from line_processor import *
 
 class Extractor(object):
     
@@ -9,129 +10,197 @@ class Extractor(object):
         self._mse = defaultdict(list)
         self._ssim = defaultdict(list)
 
-    def _extract_helper(self, path):
-        key = path.split('/')[-1]
-        with open(os.path.abspath(path), 'r') as f:
-            line = f.readline()
-            total_time, time_nums = 0, 0
-            received_files, lost_files = 0, 0
-            t_mse, t_ssim = 0, 0
+    def _get_dictionary_key(self, path):
+        return path.split('/')[-1]
 
-            while line:
-                temp_line = line.strip()
-                # extract broadcast time
-                if temp_line.startswith('Broadcasting Time'):
-                    time_nums += 1
-                    time = temp_line.split(':')[1]
-                    total_time += float(time)
-                # extract received files number
-                elif temp_line.endswith('files') or temp_line.endswith('file'):
-                    temp_string = temp_line.split(',')[1]
-                    received = temp_string.split()[0]
-                    received_files = int(received)
-                # extract lost files number
-                elif temp_line.startswith('File Loss'):
-                    lost = temp_line.split('=')[1]
-                    lost_files = int(lost)
-                # extract MSE and SSIM
-                elif temp_line.startswith('MSE'):
-                    temp_string = temp_line.split(',')
-                    mse = float(temp_string[0].split(':')[1])
-                    ssim = float(temp_string[1].split(':')[1])
-                    t_mse += mse
-                    t_ssim += ssim
-                line = f.readline()
+    def _process_single_line(self, line):
+        line_processor = LineProcessorFactory.get_line_processor(line)
+        line_processor.retrieve_data()
 
-            self._set_broadcast(key, total_time, time_nums)
-            self._set_loss_rate(key, received_files, lost_files)
-            self._set_mse_ssim(key, received_files, t_mse, t_ssim)
+    def _process_lines(self, lines):
+        for line in lines:
+            self._process_single_line(line)
 
-    def _set_broadcast(self, key, total_time, time_nums):
-        if time_nums:
-            average_time = total_time / time_nums
-            self._broadcast_time[key].append(round(average_time, 5))
+    def _reset_line_processors(self):
+        LineProcessorFactory.reset()
+
+    def _extract_from_file(self, path):
+        key = self._get_dictionary_key(path) 
+        lines = self._get_lines_from_file(os.path.abspath(path))
+        self._process_lines(lines)
+        self._set_field(key)
+        self._reset_line_processors()
+
+    def _get_lines_from_file(self, filepath):
+        with open(filepath, 'r') as f:
+            lines = [line.strip() for line in f.readlines()]
+
+        return lines
+
+    def _set_field(self, key):
+        self._set_broadcast(key)
+        self._set_loss_rate(key)
+        self._set_mse_ssim(key)
+
+    def _set_broadcast(self, key):
+        broadcast_numbers = self._get_broadcast_param()[1]
+        if broadcast_numbers:
+            self._write_broadcast_data(key)
+
+    def _write_broadcast_data(self, key):
+        total_broadcast_time, broadcast_numbers = self._get_broadcast_param()
+        average_time = total_broadcast_time / broadcast_numbers
+        self._broadcast_time[key].append(round(average_time, 5))
+
+    def _get_broadcast_param(self):
+        line_processor = LineProcessorFactory.get_broadcast_line_processor()
+
+        return (line_processor.total_broadcast_time, 
+                line_processor.lines_processed)
     
-    def _set_mse_ssim(self, key, received, tmse, tssim):
-        if received:
-            self._mse[key].append(tmse / received)
-            self._ssim[key].append(tssim / received)
+    def _get_mse_ssim_param(self):
+        line_processor = LineProcessorFactory.get_mse_ssim_line_processor()
 
-    def _set_loss_rate(self, key, received, lost):
-        desired = lost + received
-        if desired:
-            loss_rate = lost / desired
-            self._file_loss_rate[key].append(round(loss_rate, 3))
+        return (line_processor.total_mse, line_processor.total_ssim)
 
-    def _extract(self, path):
-        if os.path.isfile(path):
-            self._extract_helper(path)
-            
-        elif os.path.isdir(path):
-            for f in os.listdir(path):
-                self._extract(os.path.join(path, f))
+    def _get_file_received_param(self):
+        line_processor = LineProcessorFactory.get_file_received_line_processor()
 
+        return line_processor.files_received
 
-    def extract_to_csv_from(self, path):
-        self._extract(path)
+    def _get_file_loss_param(self):
+        line_processor = LineProcessorFactory.get_file_loss_line_processor()
 
+        return line_processor.files_lost
+
+    def _set_mse_ssim(self, key):
+        received_number = self._get_file_received_param()
+        if received_number:
+            self._write_mse_ssim_data(key)
+
+    def _write_mse_ssim_data(self, key):
+        total_mse, total_ssim = self._get_mse_ssim_param()
+        received_number = self._get_file_received_param()
+        self._mse[key].append(total_mse / received_number)
+        self._ssim[key].append(total_ssim / received_number)
+
+    def _set_loss_rate(self, key):
+        lost_number = self._get_file_loss_param()
+        received_number = self._get_file_received_param()
+        desired_number = lost_number + received_number
+        if desired_number:
+            self._write_loss_rate_data(key)
+
+    def _write_loss_rate_data(self, key):
+        lost_number = self._get_file_loss_param()
+        received_number = self._get_file_received_param()
+        desired_number = lost_number + received_number
+        loss_rate = lost_number / desired_number
+        self._file_loss_rate[key].append(round(loss_rate, 3))
+
+    def _extract_from_path(self, path):
+        if self._is_file(path):
+            self._extract_from_file(path)
+        else: 
+            self._extract_from_folder(path)
+
+    def _extract_from_folder(self, folderPath):
+        for namedFile in self._get_files_under_path(folderPath):
+            self._extract_from_path(os.path.join(folderPath, namedFile))
+
+    def _is_file(self, path):
+        return os.path.isfile(path)
+
+    def _get_files_under_path(self, path):
+        return os.listdir(path)
+
+    def extract_data_to_csv_from_folder(self, path):
+        self._extract_from_path(path)
+
+        self._write_data_to_files()
+
+    def _write_data_to_files(self):
+        self._write_broadcast_log('broadcast_time.csv')
+        self._write_file_loss_rate_log('file_loss_rate.csv')
+        self._write_mse_log('mse.csv')
+        self._write_ssim_log('ssim.csv')
+
+    def _write_broadcast_log(self, filename):
         if self._broadcast_time:
             columns = list(self._broadcast_time.keys())
             columns = ','.join(columns)
-            filename = 'broadcast.csv'
             self._write_helper(filename, columns, 0)
 
+    def _write_file_loss_rate_log(self, filename):
         if self._file_loss_rate:
             columns = list(self._file_loss_rate.keys())
             columns = ','.join(columns)
-            filename = 'file_loss_rate.csv'
             self._write_helper(filename, columns, 1)
 
+    def _write_mse_log(self, filename):
         if self._mse:
             columns = list(self._mse.keys())
             columns = ','.join(columns)
-            filename = 'mse.csv'
             self._write_helper(filename, columns, 2)
 
+    def _write_ssim_log(self, filename):
         if self._ssim:
             columns = list(self._ssim.keys())
             columns = ','.join(columns)
-            filename = 'ssim.csv'
             self._write_helper(filename, columns, 3)
 
     def _write_helper(self, filename, columns, flag):
+        self._write_header(filename, columns)
+        self._write_body(filename, flag)
+
+    def _write_header(self, filename, columns):
         with open(filename, 'w') as f:
             f.write("%s\n"%(columns))
-            values = []
-            if flag == 0:           # 0: Broadcast
-                values = list(self._broadcast_time.values())
-            elif flag == 1:         # 1: Loss_rate
-                values = list(self._file_loss_rate.values())
-            elif flag == 2:         # 2: MSE
-                values = list(self._mse.values())
-            elif flag == 3:         # 3: SSIM
-                values = list(self._ssim.values())
 
-            for i in range(len(values[0])):
-                data = []
-                for v in values:
-                    try:
-                        data.append(str(v[i]))
-                        data.append(',')
-                    except:
-                        data.append('N/A')
-                        data.append(',')
+    def _write_body(self, filename, flag):
+        values = self._get_values(flag)
+        for index_of_value in range(len(values[0])):
+            data = self._get_prepared_data(values, index_of_value)
+            self._write_prepared_data(filename, data)
 
-                del data[-1]
-                data = ''.join(data)
-                f.write("%s\n"%(data))
+    def _write_prepared_data(self, filename, data):
+        with open(filename, 'a') as f:
+            f.write("%s\n"%(data))
+
+    def _get_prepared_data(self, values, index_of_value):
+        data = []
+        for value in values:
+            try:
+                data.append(str(value[index_of_value]))
+                data.append(',')
+            except:
+                data.append('N/A')
+                data.append(',')
+
+        del data[-1]
+        data = ''.join(data)
+
+        return data
 
 
+    def _get_values(self, flag):
+        values = []
+        if flag == 0:           # 0: Broadcast
+            values = list(self._broadcast_time.values())
+        elif flag == 1:         # 1: Loss_rate
+            values = list(self._file_loss_rate.values())
+        elif flag == 2:         # 2: MSE
+            values = list(self._mse.values())
+        elif flag == 3:         # 3: SSIM
+            values = list(self._ssim.values())
+
+        return values
 
 
 def main():
     extractor = Extractor()
     print('[INFO] start extracting...')
-    extractor.extract_to_csv_from('log')
+    extractor.extract_data_to_csv_from_folder('log')
     print('[INFO] done')
     
 
---
+++ N2OTest/MQTT_expose/line_processor.py
@@ -0,0 +1,140 @@
+class LineProcessorFactory(object):
+
+    @staticmethod
+    def get_all_line_processors():
+        return [
+                BroadcastLineProcessor(),
+                FileReceivedLineProcessor(),
+                FileLossLineProcessor(),
+                MseSsimLineProcessor(),
+                ]
+
+    @staticmethod
+    def reset():
+        for processor in LineProcessorFactory.get_all_line_processors():
+            processor.reset()
+
+    def get_broadcast_line_processor():
+        return BroadcastLineProcessor()
+
+    def get_file_received_line_processor():
+        return FileReceivedLineProcessor()
+
+    def get_file_loss_line_processor():
+        return FileLossLineProcessor()
+
+    def get_mse_ssim_line_processor():
+        return MseSsimLineProcessor()
+
+    @staticmethod
+    def get_line_processor(line):
+        if line.startswith('Broadcasting Time'):
+            return BroadcastLineProcessor(line)
+
+        elif line.endswith('files') or line.endswith('file'):
+            return FileReceivedLineProcessor(line)
+
+        elif line.startswith('File Loss'):
+            return FileLossLineProcessor(line)
+
+        elif line.startswith('MSE'):
+            return MseSsimLineProcessor(line)
+        else:
+            return DefaultLineProcessor()
+
+class DefaultLineProcessor(object):
+
+    def retrieve_data(self):
+        pass
+
+
+class BroadcastLineProcessor(object):
+
+    total_broadcast_time = 0
+    lines_processed = 0
+
+    def __init__(self, line=None):
+        self.line = line
+
+    def retrieve_data(self):
+        BroadcastLineProcessor.lines_processed += 1
+        time = self._get_time()
+        BroadcastLineProcessor.total_broadcast_time += time
+
+    def _get_time(self):
+        return float(self.line.split(':')[1])
+
+    def reset(self):
+        BroadcastLineProcessor.total_broadcast_time = 0
+        BroadcastLineProcessor.lines_processed = 0
+
+class FileReceivedLineProcessor(object):
+
+    files_received = 0
+
+    def __init__(self, line=None):
+        self.line = line
+
+    def retrieve_data(self):
+        FileReceivedLineProcessor.files_received = self._get_files_number()
+
+    def _get_files_number(self):
+        files_string = self.line.split(',')[1]
+        files_number = files_string.split()[0]
+        files_number = int(files_number)
+
+        return files_number
+
+    def reset(self):
+        FileReceivedLineProcessor.files_received = 0
+
+class FileLossLineProcessor(object):
+
+    files_lost = 0
+
+    def __init__(self, line=None):
+        self.line = line
+
+    def retrieve_data(self):
+        FileLossLineProcessor.files_lost = self._get_lost_number()
+
+    def _get_lost_number(self):
+        return int(self.line.split('=')[1])
+
+    def reset(self):
+        FileLossLineProcessor.files_lost = 0
+
+class MseSsimLineProcessor(object):
+
+    total_mse = 0
+    total_ssim = 0
+
+    def __init__(self, line=None):
+        self.line = line
+
+    def retrieve_data(self):
+        MseSsimLineProcessor.total_mse += self._get_mse()
+        MseSsimLineProcessor.total_ssim += self._get_ssim()
+
+    def _get_mse(self):
+        mse_string = self._get_mse_string()
+        mse = float(mse_string.split(':')[1])
+        
+        return mse
+
+    def _get_mse_string(self):
+        return self.line.split(',')[0]
+
+    def _get_ssim(self):
+        ssim_string = self._get_ssim_string()
+        ssim = float(ssim_string.split(':')[1])
+
+        return ssim
+
+    def _get_ssim_string(self):
+        return self.line.split(',')[1]
+
+    def reset(self):
+        MseSsimLineProcessor.total_mse = 0
+        MseSsimLineProcessor.total_ssim = 0
+
